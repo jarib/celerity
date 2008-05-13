d5305028dffaba078f8f16596159d97ea1ccb9b2
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-05-13 10:59:42 +0000

    Remove a duplicate spec
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@47 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/spec/element_spec.rb
+++ trunk/spec/element_spec.rb
@@ -23,15 +23,11 @@ describe "Element" do
     end
   end
   
-
   describe "#method_missing" do
     it "should magically return the requested attribute if the attribute is defined in the attribute list" do
       @ie.form(:index, 1).action.should == 'forms_with_input_elements.html'
     end
-    it "should call the super class method if the attribute is not defined in the element's attribute list" do
-      @ie.form(:index, 2).text.should == 'Username Username 1 Delete'
-    end
-    it "should raise NoMethodError if the requested method is not defined in the super class" do
+    it "should raise NoMethodError if the requested method isn't among the attributes" do
       lambda { @ie.button(:index, 1).no_such_attribute_or_method }.should raise_error(NoMethodError)
     end
   end
