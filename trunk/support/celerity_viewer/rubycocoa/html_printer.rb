79ecbef3665f980cbfd4277feec86608cd125fde
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-05-23 13:04:59 +0000

    Remove an empty file
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@147 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/support/celerity_viewer/rubycocoa/html_printer.rb
+++
@@ -1,8 +0,0 @@
-# 
-#  html_printer.rb
-#  trunk
-#  
-#  Created by Jari Bakken on 2008-05-14.
-#  Copyright 2008 Jari Bakken. All rights reserved.
-# 
-#  Standalone script for printing a WebKit rendering of the given HTML.
