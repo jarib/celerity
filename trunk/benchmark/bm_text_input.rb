5521d01d5a7384fcce9c9e310f231b9e6b6aa4ba
arex <arex@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-05-29 13:48:00 +0000

    Benchmarking TextField#set against TextField#value= :-)
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@168 f2a54740-6eb5-4f7c-93c5-883597f6e056

---
+++ trunk/benchmark/bm_text_input.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + "/loader"
+
+browser = FRAMEWORK::IE.new
+browser.goto(TEST_HOST + "/forms_with_input_elements.html")
+
+TESTS = 10000
+res = Benchmark.bmbm do |results|
+  results.report("TextField#set") do
+    TESTS.times { browser.text_field(:id, "new_user_first_name").set("1234567890") }
+  end
+  results.report("TextField#value=") do
+    TESTS.times { browser.text_field(:id, "new_user_first_name").value = "1234567890" }
+  end
+end
+
+puts
+total = res.inject(0.0) { |mem, bm| mem + bm.real }
+puts "total  : " + total.to_s
+puts "average: " + (total/res.size.to_f).to_s
