47e45969ede2f7e535bc5b4d63a2c717f4fd9b34
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-08-19 14:15:47 +0000

    Add docs for ClickableElement#click_and_attach and bump version number.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@241 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/lib/celerity/clickable_element.rb
+++ trunk/lib/celerity/clickable_element.rb
@@ -9,6 +9,11 @@ module Celerity
       @container.update_page(@object.click)
     end
 
+    # Click the element and return a new Browser instance with the resulting page.
+    # This is useful for elements that trigger popups windows when clicked.
+    #
+    # @return [Celerity::Browser]
+    # @since 0.0.5
     def click_and_attach
       assert_exists
       assert_enabled if respond_to?(:assert_enabled)
