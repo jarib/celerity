85623f0c872c81efb285664b24c9281e0f18ca1a
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-12-09 19:25:34 +0000

    Delete support/celerity_viewer stuff - moved to own repo
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@352 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/support/celerity_viewer/java/lib/celerity_viewer/version.rb
+++
@@ -1,9 +0,0 @@
-module CelerityViewer #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 0
-    TINY  = 1
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
-end
--- trunk/support/celerity_viewer/rubycocoa/main.m
+++
@@ -1,7 +0,0 @@
-#import <RubyCocoa/RBRuntime.h>
-
-int main(int argc, const char* argv[])
-{
-	return RBApplicationMain("main.rb", argc, argv);
-}
-
