2616622c26445b423138c77ef3b0f013a0925793
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-12-12 19:48:46 +0000

    Forgot to wrap meta element in the Celerity module
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@363 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/lib/celerity/elements/meta.rb
+++ trunk/lib/celerity/elements/meta.rb
@@ -1,5 +1,6 @@
-class Meta < Celerity::Element
-  ATTRIBUTES = [:name, :id, :'http-equiv', :content, :scheme] | HTML_401_TRANSITIONAL[:i18n]
-  TAGS = [ Identifier.new('meta') ]
+module Celerity
+  class Meta < Element
+    ATTRIBUTES = [:name, :id, :'http-equiv', :content, :scheme] | HTML_401_TRANSITIONAL[:i18n]
+    TAGS = [ Identifier.new('meta') ]
+  end
 end
-
