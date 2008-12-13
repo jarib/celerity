77fd5a93de7e9797681983687c191702567076e8
jarib <jarib@f2a54740-6eb5-4f7c-93c5-883597f6e056>
2008-12-13 16:00:20 +0000

    alphabetize container.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/celerity@366 f2a54740-6eb5-4f7c-93c5-883597f6e056

--- trunk/lib/celerity/container.rb
+++ trunk/lib/celerity/container.rb
@@ -64,92 +64,32 @@ module Celerity
     def update_page(page)
       @browser.page = page
     end
-
-    # @return [Celerity::Frame]
-    def frame(*args)
-      Frame.new(self, *args)
-    end
-
-    # @return [Celerity::Frames]
-    def frames
-      Frames.new(self)
-    end
-
-    # @return [Celerity::Table]
-    def table(*args)
-      Table.new(self, *args)
-    end
-
-    # @return [Celerity::Tables]
-    def tables
-      Tables.new(self)
-    end
-
-    # @return [Celerity::TableHeader]
-    def thead(*args)
-      TableHeader.new(self, *args)
-    end
-
-    # @return [Celerity::TableHeaders]
-    def theads
-      TableHeaders.new(self)
-    end
-
-    # @return [Celerity::TableBody]
-    def tbody(*args)
-      TableBody.new(self, *args)
-    end
-
-    # @return [Celerity::TableBodies]
-    def tbodies
-      TableBodies.new(self)
-    end
-
-    # @return [Celerity::TableFooter]
-    def tfoot(*args)
-      TableFooter.new(self, *args)
-    end
-
-    # @return [Celerity::TableFooters]
-    def tfoots
-      TableFooters.new(self)
-    end
-    alias_method :tfeet, :tfoots # :-)
-
-    # @return [Celerity::TableCell]
-    def cell(*args)
-      TableCell.new(self, *args)
-    end
-
-    # @return [Celerity::TableCells]
-    def cells
-      TableCells.new(self)
-    end
-
-    # Watir's cells() won't return <th> elements.
-    # This is a workaround.
+    
+    # Used internally.
     #
-    # @return [Celerity::Th]
-    def th(*args)
-      Th.new(self, *args)
+    # @param [String] string The string to match against.
+    # @param [Regexp, String, #to_s] what The match we're looking for.
+    # @return [MatchData, true, false, nil]
+    #
+    # @api private
+    def matches?(string, what)
+      Regexp === what ? string =~ what : string == what.to_s
     end
 
-    # TODO: implement or change api,
-    # @see th
-    def ths
-      raise NotImplementedError
+    #
+    # below methods sorted alphabetically
+    # 
+
+    # @return [Celerity::Area]
+    def area(*args)
+      Area.new(self, *args)
     end
 
-    # @return [Celerity::TableRow]
-    def row(*args)
-      TableRow.new(self, *args)
+    # @return [Celerity::Areas]
+    def areas
+      Areas.new(self)
     end
-
-    # @return [Celerity::TableRows]
-    def rows
-      TableRows.new(self)
-    end
-
+    
     # @return [Celerity::Button]
     def button(*args)
       Button.new(self, *args)
@@ -160,44 +100,14 @@ module Celerity
       Buttons.new(self)
     end
 
-    # @return [Celerity::FileField]
-    def file_field(*args)
-      FileField.new(self, *args)
+    # @return [Celerity::TableCell]
+    def cell(*args)
+      TableCell.new(self, *args)
     end
 
-    # @return [Celerity::FileFields]
-    def file_fields
-      FileFields.new(self)
-    end
-
-    # @return [Celerity::TextField]
-    def text_field(*args)
-      TextField.new(self, *args)
-    end
-
-    # @return [Celerity::TextFields]
-    def text_fields
-      TextFields.new(self)
-    end
-
-    # @return [Celerity::Hidden]
-    def hidden(*args)
-      Hidden.new(self, *args)
-    end
-
-    # @return [Celerity::Hiddens]
-    def hiddens
-      Hiddens.new(self)
-    end
-
-    # @return [Celerity::SelectList]
-    def select_list(*args)
-      SelectList.new(self, *args)
-    end
-
-    # @return [Celerity::SelectLists]
-    def select_lists
-      SelectLists.new(self)
+    # @return [Celerity::TableCells]
+    def cells
+      TableCells.new(self)
     end
 
     # Since finding checkboxes by value is very common, you can use this shorthand:
@@ -218,116 +128,14 @@ module Celerity
       CheckBoxes.new(self)
     end
 
-    # Since finding radios by value is very common, you can use this shorthand:
-    #
-    #   browser.radio(:name, 'a_name', '1234').set
-    #
-    # or
-    #
-    #   browser.radio(:name => 'a_name', :value => '1234').set
-    #
-    # @return [Celerity::Radio]
-    def radio(*args)
-      Radio.new(self, *args)
-    end
-
-    # @return [Celerity::Radios]
-    def radios
-      Radios.new(self)
-    end
-
-    # @return [Celerity::Link]
-    def link(*args)
-      Link.new(self, *args)
-    end
-
-    # @return [Celerity::Links]
-    def links
-      Links.new(self)
-    end
-
-    # @return [Celerity::Ul]
-    def ul(*args)
-      Ul.new(self, *args)
-    end
-
-    # @return [Celerity::Uls]
-    def uls
-      Uls.new(self)
-    end
-
-    # @return [Celerity::Ol]
-    def ol(*args)
-      Ol.new(self, *args)
-    end
-
-    # @return [Celerity::Ols]
-    def ols
-      Ols.new(self)
-    end
-
-    def dl(*args)
-      Dl.new(self, *args)
-    end
-
-    def dls
-      Dls.new(self)
-    end
-
-    def dt(*args)
-      Dt.new(self, *args)
-    end
-
-    def dts
-      Dts.new(self)
-    end
-
+    # @return [Celerity::Dd]
     def dd(*args)
       Dd.new(self, *args)
     end
 
+    # @return [Celerity::Dds]
     def dds
       Dds.new(self)
-    end
-
-    # @return [Celerity::Li]
-    def li(*args)
-      Li.new(self, *args)
-    end
-
-    # @return [Celerity::Lis]
-    def lis
-      Lis.new(self)
-    end
-
-    # @return [Celerity::Map]
-    def map(*args)
-      Map.new(self, *args)
-    end
-
-    # @return [Celerity::Maps]
-    def maps
-      Maps.new(self)
-    end
-
-    # @return [Celerity::Area]
-    def area(*args)
-      Area.new(self, *args)
-    end
-
-    # @return [Celerity::Areas]
-    def areas
-      Areas.new(self)
-    end
-
-    # @return [Celerity::Image]
-    def image(*args)
-      Image.new(self, *args)
-    end
-
-    # @return [Celerity::Images]
-    def images
-      Images.new(self)
     end
 
     # @return [Celerity::Div]
@@ -340,6 +148,36 @@ module Celerity
       Divs.new(self)
     end
 
+    # @return [Celerity::Dl]
+    def dl(*args)
+      Dl.new(self, *args)
+    end
+
+    # @return [Celerity::Dls]
+    def dls
+      Dls.new(self)
+    end
+
+    # @return [Celerity::Dt]
+    def dt(*args)
+      Dt.new(self, *args)
+    end
+
+    # @return [Celerity::Dts]
+    def dts
+      Dts.new(self)
+    end
+
+    # @return [Celerity::FileField]
+    def file_field(*args)
+      FileField.new(self, *args)
+    end
+
+    # @return [Celerity::FileFields]
+    def file_fields
+      FileFields.new(self)
+    end
+
     # @return [Celerity::Form]
     def form(*args)
       Form.new(self, *args)
@@ -350,19 +188,157 @@ module Celerity
       Forms.new(self)
     end
 
+    # @return [Celerity::Frame]
+    def frame(*args)
+      Frame.new(self, *args)
+    end
+
+    # @return [Celerity::Frames]
+    def frames
+      Frames.new(self)
+    end
+
+    # @return [Celerity::H1]
+    def h1(*args)
+      H1.new(self, *args)
+    end
+
+    # @return [Celerity::H1s]
+    def h1s
+      H1s.new(self)
+    end
+
+    # @return [Celerity::H2]
+    def h2(*args)
+      H2.new(self, *args)
+    end
+
+    # @return [Celerity::H2s]
+    def h2s
+      H2s.new(self)
+    end
+
+    # @return [Celerity::H3]
+    def h3(*args)
+      H3.new(self, *args)
+    end
+
+    # @return [Celerity::H3s]
+    def h3s
+      H3s.new(self)
+    end
+    
+    # @return [Celerity::H4]
+    def h4(*args)
+      H4.new(self, *args)
+    end
+
+    # @return [Celerity::H4s]
+    def h4s
+      H4s.new(self)
+    end
+    
+    # @return [Celerity::H5]
+    def h5(*args)
+      H5.new(self, *args)
+    end
+
+    # @return [Celerity::H5s]
+    def h5s
+      H5s.new(self)
+    end
+    
+    # @return [Celerity::H6]
+    def h6(*args)
+      H6.new(self, *args)
+    end
+
+    # @return [Celerity::H6s]
+    def h6s
+      H6s.new(self)
+    end
+
+    # @return [Celerity::Hidden]
+    def hidden(*args)
+      Hidden.new(self, *args)
+    end
+
+    # @return [Celerity::Hiddens]
+    def hiddens
+      Hiddens.new(self)
+    end
+
+    # @return [Celerity::Image]
+    def image(*args)
+      Image.new(self, *args)
+    end
+
+    # @return [Celerity::Images]
+    def images
+      Images.new(self)
+    end
+
+    # @return [Celerity::Label]
+    def label(*args)
+      Label.new(self, *args)
+    end
+
+    # @return [Celerity::Labels]
+    def labels
+      Labels.new(self)
+    end
+
+    # @return [Celerity::Li]
+    def li(*args)
+      Li.new(self, *args)
+    end
+
+    # @return [Celerity::Lis]
+    def lis
+      Lis.new(self)
+    end
+
+    # @return [Celerity::Link]
+    def link(*args)
+      Link.new(self, *args)
+    end
+
+    # @return [Celerity::Links]
+    def links
+      Links.new(self)
+    end
+
+    # @return [Celerity::Map]
+    def map(*args)
+      Map.new(self, *args)
+    end
+
+    # @return [Celerity::Maps]
+    def maps
+      Maps.new(self)
+    end
+
+    def meta(*args)
+      Meta.new(self, *args)
+    end
+
+    def metas(*args)
+      Metas.new(self, *args)
+    end
+
+    # @return [Celerity::Ol]
+    def ol(*args)
+      Ol.new(self, *args)
+    end
+
+    # @return [Celerity::Ols]
+    def ols
+      Ols.new(self)
+    end
+
     # @return [Celerity::Option]
     def option(*args)
       Option.new(self, *args)
-    end
-
-    # @return [Celerity::Span]
-    def span(*args)
-      Span.new(self, *args)
-    end
-
-    # @return [Celerity::Spans]
-    def spans
-      Spans.new(self)
     end
 
     # @return [Celerity::P]
@@ -384,94 +360,128 @@ module Celerity
     def pres
       Pres.new(self)
     end
-
-    # @return [Celerity::Label]
-    def label(*args)
-      Label.new(self, *args)
-    end
-
-    # @return [Celerity::Labels]
-    def labels
-      Labels.new(self)
-    end
-
-    def meta(*args)
-      Meta.new(self, *args)
-    end
-
-    def metas(*args)
-      Metas.new(self, *args)
-    end
-
-    # @return [Celerity::H1]
-    def h1(*args)
-      H1.new(self, *args)
-    end
-
-    # @return [Celerity::H2]
-    def h2(*args)
-      H2.new(self, *args)
-    end
-
-    # @return [Celerity::H3]
-    def h3(*args)
-      H3.new(self, *args)
-    end
-
-    # @return [Celerity::H4]
-    def h4(*args)
-      H4.new(self, *args)
-    end
-
-    # @return [Celerity::H5]
-    def h5(*args)
-      H5.new(self, *args)
-    end
-
-    # @return [Celerity::H6]
-    def h6(*args)
-      H6.new(self, *args)
-    end
-
-    # @return [Celerity::H1s]
-    def h1s
-      H1s.new(self)
-    end
-
-    # @return [Celerity::H2s]
-    def h2s
-      H2s.new(self)
-    end
-
-    # @return [Celerity::H3s]
-    def h3s
-      H3s.new(self)
-    end
-
-    # @return [Celerity::H4s]
-    def h4s
-      H4s.new(self)
-    end
-
-    # @return [Celerity::H5s]
-    def h5s
-      H5s.new(self)
-    end
-
-    # @return [Celerity::H6s]
-    def h6s
-      H6s.new(self)
-    end
-
-    # Used internally.
+    
+    # Since finding radios by value is very common, you can use this shorthand:
     #
-    # @param [String] string The string to match against.
-    # @param [Regexp, String, #to_s] what The match we're looking for.
-    # @return [MatchData, true, false, nil]
+    #   browser.radio(:name, 'a_name', '1234').set
     #
-    # @api private
-    def matches?(string, what)
-      Regexp === what ? string =~ what : string == what.to_s
+    # or
+    #
+    #   browser.radio(:name => 'a_name', :value => '1234').set
+    #
+    # @return [Celerity::Radio]
+    def radio(*args)
+      Radio.new(self, *args)
+    end
+
+    # @return [Celerity::Radios]
+    def radios
+      Radios.new(self)
+    end
+
+    # @return [Celerity::TableRow]
+    def row(*args)
+      TableRow.new(self, *args)
+    end
+
+    # @return [Celerity::TableRows]
+    def rows
+      TableRows.new(self)
+    end
+
+    # @return [Celerity::SelectList]
+    def select_list(*args)
+      SelectList.new(self, *args)
+    end
+
+    # @return [Celerity::SelectLists]
+    def select_lists
+      SelectLists.new(self)
+    end
+    
+    # @return [Celerity::Span]
+    def span(*args)
+      Span.new(self, *args)
+    end
+
+    # @return [Celerity::Spans]
+    def spans
+      Spans.new(self)
+    end
+
+    # @return [Celerity::Table]
+    def table(*args)
+      Table.new(self, *args)
+    end
+
+    # @return [Celerity::Tables]
+    def tables
+      Tables.new(self)
+    end
+
+    # @return [Celerity::TableBody]
+    def tbody(*args)
+      TableBody.new(self, *args)
+    end
+
+    # @return [Celerity::TableBodies]
+    def tbodies
+      TableBodies.new(self)
+    end
+    
+    # @return [Celerity::TextField]
+    def text_field(*args)
+      TextField.new(self, *args)
+    end
+
+    # @return [Celerity::TextFields]
+    def text_fields
+      TextFields.new(self)
+    end
+    
+    # @return [Celerity::TableFooter]
+    def tfoot(*args)
+      TableFooter.new(self, *args)
+    end
+
+    # @return [Celerity::TableFooters]
+    def tfoots
+      TableFooters.new(self)
+    end
+    alias_method :tfeet, :tfoots # :-)
+    
+    # Watir's cells() won't return <th> elements.
+    # This is a workaround.
+    #
+    # @return [Celerity::Th]
+    def th(*args)
+      Th.new(self, *args)
+    end
+
+    # TODO: implement or change api,
+    # @see th
+    def ths
+      raise NotImplementedError
+    end
+    
+    # @return [Celerity::TableHeader]
+    def thead(*args)
+      TableHeader.new(self, *args)
+    end
+
+    # @return [Celerity::TableHeaders]
+    def theads
+      TableHeaders.new(self)
+    end
+
+    # @return [Celerity::Ul]
+    def ul(*args)
+      Ul.new(self, *args)
+    end
+
+    # @return [Celerity::Uls]
+    def uls
+      Uls.new(self)
     end
 
   end # Container
