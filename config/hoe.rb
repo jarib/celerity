93822fa7ebd890b271fd73b88c0f28a3d1cb3c97
Jari Bakken <jari.bakken@gmail.com>
2009-01-27 20:36:39 +0100

    Add classes/ to the clean globs

--- config/hoe.rb
+++ config/hoe.rb
@@ -52,7 +52,7 @@ $hoe = Hoe.new(GEM_NAME, VERS) do |p|
   p.url = HOMEPATH
   p.rubyforge_name = RUBYFORGE_PROJECT if RUBYFORGE_PROJECT
   p.test_globs = ["test/**/test_*.rb"]
-  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store']  #An array of file patterns to delete on clean.
+  p.clean_globs |= ['**/.*.sw?', '*.gem', '.config', '**/.DS_Store', 'classes']  #An array of file patterns to delete on clean.
 
   # make sure rdoc doesn't see any jars
   p.rdoc_pattern = /^(lib|bin|ext).+\.(rb|txt|)$|txt$/
