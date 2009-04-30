1fbbc298429772d6f20df8ad9501134df51d0943
Jari Bakken <jari.bakken@gmail.com>
2009-04-30 17:41:48 +0200

    Work around weird RSpec (or whatever) issue
    The spec run stopped after 170 examples (with no failures) when using Kernel#open on a file:// path.

--- spec/clickable_element_spec.rb
+++ spec/clickable_element_spec.rb
@@ -9,25 +9,23 @@ describe "ClickableElement" do
   before :each do
     @browser.goto(HTML_DIR + "/non_control_elements.html")
   end
-  
+
   describe "#click_and_attach" do
-    
-    it 'should open a page in a browser with the same options and cookies' do
+    it 'opens a page in a browser with the same options and cookies' do
       browser = @browser.link(:name, /bad_attribute/).click_and_attach
       browser.text.include?("User administration").should be_true # to make sure it is open.
       browser.options.should == @browser.options
       browser.cookies.should == @browser.cookies
     end
-    
   end
-  
+
   describe "#download" do
-    
-    it 'should return a click-opened page as io' do
-      @browser.link(:name, /bad_attribute/).download.read.should == open(HTML_DIR + "forms_with_input_elements.html").read
-      @browser.link(:name, /bad_attribute/).should be_true # to make sure the browser is still on the same page.
+    it 'returns a click-opened page as io' do
+      expected = File.read "#{HTML_DIR}/forms_with_input_elements.html".sub("file://", '')
+      
+      @browser.link(:name, /bad_attribute/).download.read.should == expected
+      @browser.link(:name, /bad_attribute/).should exist
     end
-    
   end
-  
+
 end
