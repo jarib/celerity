6f041291867db6a20a655b43b76f4a9cddc42e23
Jari Bakken <jari.bakken@gmail.com>
2009-08-07 18:16:31 +0200

    Add Sinatra as a development dependency

--- config/hoe.rb
+++ config/hoe.rb
@@ -57,6 +57,7 @@ $hoe = Hoe.spec(GEM_NAME) do |p|
 
   # == Optional
   p.changes = p.paragraphs_of("History.txt", 0..1).join("\n\n")
+  p.extra_dev_deps = [ ['sinatra', '>= 0.9.4']  ]
   #p.extra_deps = []     # An array of rubygem dependencies [name, version], e.g. [ ['active_support', '>= 1.3.1'] ]
   #p.spec_extras = {}    # A hash of extra values to set in the gemspec.
 
