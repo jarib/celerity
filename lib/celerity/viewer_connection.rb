fda5981fa940ee4b0e63e9dcc8df50749564d40b
Jari Bakken <jari.bakken@gmail.com>
2009-08-13 12:20:58 +0200

    Document ViewerConnection and add ViewerConnection#save_render_tree

--- lib/celerity/viewer_connection.rb
+++ lib/celerity/viewer_connection.rb
@@ -1,7 +1,12 @@
 module Celerity
   class ViewerConnection
 
+    #
+    # Create a new connection to the given host/port
+    #
+
     def self.create(host, port)
+      # if the connection fails, we won't spend time loading json
       socket = TCPSocket.new(host, port)
       require "json"
       new(socket)
@@ -11,13 +16,35 @@ module Celerity
       @socket = socket
     end
 
+    #
+    # Tells the viewer to render the given HTML, with the given URL as base url.
+    #
+
     def render_html(html, url)
       send_data({'method' => 'page_changed', 'html' => html, 'url' => url}.to_json)
     end
 
-    def save(path = nil)
+    #
+    # Tells the viewer to save a screenshot of the current page to the given path.
+    # May not be available on all viewers.
+    #
+
+    def save(path)
       send_data({'method' => 'save', 'path' => path}.to_json)
     end
+
+    #
+    # Tells the viewer to dump the render tree to the given path.
+    # Only available on the Qt viewer.
+    #
+
+    def save_render_tree(path)
+      send_data({'method' => 'save_render_tree', 'path' => path}.to_json)
+    end
+
+    #
+    # Close the connection.
+    #
 
     def close
       @socket.close rescue nil
@@ -29,6 +56,8 @@ module Celerity
       data = "Content-Length: #{json.size}\n\n#{json}"
       @socket.write data
       @socket.flush
+
+      nil
     end
 
   end
