3ebddb7223034e8bc41cba6efe9e52756504a16e
Jari Bakken <jari.bakken@gmail.com>
2009-10-11 01:13:57 +0200

    And click_and_attach spec (removed from WatirSpec)

---
+++ spec/button_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/watirspec/spec_helper'
+
+describe "Button" do
+
+  before :each do
+    browser.goto(WatirSpec.files + "/forms_with_input_elements.html")
+  end
+
+  describe "#click_and_attach" do
+    it "returns a new browser instance with the popup page" do
+      b = browser.button(:name, 'new_popup_window').click_and_attach
+      b.should_not == browser
+      b.title.should == "Tables"
+      browser.title.should == "Forms with input elements"
+    end
+
+    it "returns a new browser with same log_level as original" do
+      b = browser.button(:name, 'new_popup_window').click_and_attach
+      b.should_not == browser
+      b.log_level.should == browser.log_level
+    end
+  end
+
+end
