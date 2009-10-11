fb15f9dfa994407c107b6087bfa81b6d28e18099
Jari Bakken <jari.bakken@gmail.com>
2009-10-12 00:35:48 +0200

    Add click_no_wait spec + update watirspec to ab04e8ff

---
+++ spec/watir_compatibility_spec.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + "/watirspec/spec_helper"
+
+describe "Button" do
+
+  before :each do
+    browser.goto(WatirSpec.host + "/forms_with_input_elements.html")
+  end
+
+  describe "#click_no_wait" do
+    deviates_on :celerity do
+      it "behaves like #click" do
+        browser.button(:id, 'new_user_submit').click_no_wait
+        browser.text.should include("You posted the following content:")
+      end
+    end
+  end
+
+end
+
+describe "Link" do
+
+  before :each do
+    browser.goto(WatirSpec.host + "/non_control_elements.html")
+  end
+
+  describe "#click_no_wait" do
+    it "behaves like #click" do
+      browser.link(:text, "Link 3").click_no_wait
+      browser.text.include?("User administration").should be_true
+    end
+  end
+
+end
+
