75d51fec36a93818eb66239942b13c608026d1e7
Jari Bakken <jari.bakken@gmail.com>
2010-01-19 19:51:14 +0100

    Moved spec from watirspec

---
+++ spec/table_spec.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "Table" do
+
+  before :each do
+    browser.goto(WatirSpec.files + "/tables.html")
+  end
+
+  describe "#locate" do
+    it "is not nil for existing tables" do
+      browser.table(:id, 'axis_example').locate.should_not be_nil
+    end
+  end
+  
+end
+
