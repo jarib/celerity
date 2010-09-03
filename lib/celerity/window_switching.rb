ec226a1ef6088f75db7f8e0ae65af6309917b1e0
Jari Bakken <jari.bakken@gmail.com>
2010-09-03 14:59:24 +0200

    More work on window switching.

--- lib/celerity/window_switching.rb
+++ lib/celerity/window_switching.rb
@@ -2,39 +2,48 @@ module Celerity
   module WindowSwitching
 
     def windows(*args)
-      all = @webclient.getWebWindows.map { |w| Window.new(self, w) }
-      
+      all = all_windows
+
       if args.empty?
         all
       else
         filter_windows(args, all, :select)
       end
     end
-    
+
     def window(*args, &blk)
       win = filter_windows(args, windows, :find)
-      
+
       if win && block_given?
         win.use(&blk)
       end
-      
+
       win
     end
-    
+
     private
-    
+
+    def all_windows
+      @webclient.getTopLevelWindows.map { |w| Window.new(self, w) }
+    end
+
     def filter_windows(args, all, method)
       sel = extract_selector(args)
-      
+      sel.delete(:index)
+
+      if sel.empty?
+        return all.find { |e| e.current? }
+      end
+
       unless sel.keys.all? { |k| [:title, :url].include? k }
-        raise ArgumentError, "invalid window selector: #{sel}"
+        raise ArgumentError, "invalid window selector: #{sel.inspect}"
       end
 
       all.send(method) do |win|
         sel.all? { |key, value| value === win.send(key) }
       end
     end
-    
+
   end # WindowSwitching
 
   class Window
@@ -43,37 +52,57 @@ module Celerity
       @window = window
     end
 
+    def inspect
+      '#<%s:0x%x id=%s>' % [self.class, hash*2, @window.to_s]
+    end
+
     def current?
-      @browser.page == @window.getEnclosedPage
+      @browser.webclient.getCurrentWindow == @window
     end
 
     def close
-      raise NotImplementedError, "#{self.class}#close"
+      @window.close
+      set_window first_non_closed_window
     end
 
     def title
-      use { @browser.title }
+      @window.getEnclosedPage.getTitleText
     end
 
     def url
-      use { @browser.url }
+      @window.getEnclosedPage.getWebResponse.getWebRequest.getUrl.toString
     end
 
     def use(&blk)
-      old_window = @browser.webclient.getCurrentWindow
-      
-      @browser.webclient.setCurrentWindow(@window)
-      @browser.page = @window.getEnclosedPage
-      
+      return if current?
+
       if block_given?
+        old_window = current_window
+        set_window @window
         res = yield
-        @browser.webclient.setCurrentWindow(old_window)
-        @browser.page = old_window.getEnclosedPage
-        
+        set_window old_window
+
         return res
       end
-      
+
+      set_window @window
       self
+    end
+
+    private
+
+    def set_window(window)
+      @browser.webclient.setCurrentWindow(window)
+      @browser.page = window.getEnclosedPage
+    end
+
+    def current_window
+      @browser.webclient.getCurrentWindow.getTopWindow
+    end
+
+    def first_non_closed_window
+      win = @browser.webclient.getWebWindows.find { |w| !w.isClosed }
+      win or raise Exception::CelerityException, "no windows left open"
     end
   end # Window
 end # Celerity
