f8c7adcab34de5003294b367a4b7fed40389e248
Jon Druse <jon@jondruse.com>
2010-09-23 13:55:45 -0700

    fixing doc

--- lib/celerity/elements/button.rb
+++ lib/celerity/elements/button.rb
@@ -5,7 +5,7 @@ module Celerity
   #
   # Class representing button elements.
   #
-  # This class covers both <button> and <input type="submit|reset|image|button" /> elements.
+  # This class covers both button tags and input tags with types submit|reset|image|button.
   #
 
   class Button < InputElement
