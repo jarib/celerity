a43e5c2bee3a7fc8beead486403f202aaf96c64b
Jon Druse <jon@jondruse.com>
2011-02-22 11:18:41 -0800

    add default wait duration

--- lib/celerity/browser.rb
+++ lib/celerity/browser.rb
@@ -3,7 +3,7 @@ module Celerity
     include Container
     include XpathSupport
 
-    attr_accessor :page, :object, :charset
+    attr_accessor :page, :object, :charset, :wait_milisec
     attr_reader :webclient, :viewer, :options
 
     #
@@ -277,7 +277,8 @@ module Celerity
     # Wait for javascript jobs to finish
     #
 
-    def wait(sec=10000)
+    def wait(sec=nil)
+      sec ||= wait_milisec
       assert_exists
       @webclient.waitForBackgroundJavaScript(sec);
     end
@@ -859,6 +860,7 @@ module Celerity
       self.ignore_pattern         = opts.delete(:ignore_pattern) if opts[:ignore_pattern]
       self.refresh_handler        = opts.delete(:refresh_handler) if opts[:refresh_handler]
       self.cache_limit            = opts.delete(:cache_limit) if opts[:cache_limit]
+      self.wait_milisec           = opts.delete(:wait_milisec) || 10000
 
       if opts.delete(:resynchronize)
         controller = ::HtmlUnit::NicelyResynchronizingAjaxController.new
