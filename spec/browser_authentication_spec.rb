0e4cc1ab3580d1b098f80b983703c96b9017de6e
Jari Bakken <jari.bakken@gmail.com>
2011-12-20 00:51:37 +0100

    Try to figure out why the build is failing on travis only.

--- spec/browser_authentication_spec.rb
+++ spec/browser_authentication_spec.rb
@@ -4,9 +4,11 @@ describe "Browser" do
 
   describe "#credentials=" do
     it "sets the basic authentication credentials" do
-      browser.goto(WatirSpec.host + "/authentication")
-      browser.text.should_not include("ok")
+      p :status_code_exceptions => browser.status_code_exceptions
 
+      browser.goto(WatirSpec.host + "/authentication")
+
+      browser.text.should_not include("ok")
       browser.credentials = "foo:bar"
 
       browser.goto(WatirSpec.host + "/authentication")
