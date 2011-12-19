4426468bf05d38ec58744b2414d7d31a91a7926d
Hiro Asari <asari.ruby@gmail.com>
2011-12-19 05:25:12 -0500

    Tweak short_inspect for 1.9 mode compatibility. In 1.9 mode, ivars are Symbols, not Strings.

--- lib/celerity/short_inspect.rb
+++ lib/celerity/short_inspect.rb
@@ -3,8 +3,14 @@ module Celerity
 
     def short_inspect(opts)
       if excluded_ivars = opts[:exclude]
+        if is_ruby19
+          excluded_ivars.map! { |ivar| ivar.to_sym }
+        end
         ivars = (instance_variables - excluded_ivars)
       elsif included_ivars = opts[:include]
+        if is_ruby19
+          included_ivars.map! { |ivar| ivar.to_sym }
+        end
         ivars = included_ivars
       else
         raise ArgumentError, "unknown arg: #{opts.inspect}"
@@ -14,7 +20,10 @@ module Celerity
       '#<%s:0x%s %s>' % [self.class.name, self.hash.to_s(16), ivars.join(" ")]
     end
 
-
+    private
+    def is_ruby19
+      RUBY_VERSION >= "1.9"
+    end
 
   end
 end
